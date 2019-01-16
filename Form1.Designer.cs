fdee56e7b7155c23953cd032f867b87ab586a6a7
Jewoo <whwpdn@gmail.com>
2019-01-16 11:39:52 +0900

    first commit
    d

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace ffmpegTestApp
+{
+    partial class ConcatForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listClips = new System.Windows.Forms.ListBox();
+            this.tbOutputPath = new System.Windows.Forms.TextBox();
+            this.btnOutBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbResolution = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listHistory = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listClips
+            // 
+            this.listClips.FormattingEnabled = true;
+            this.listClips.ItemHeight = 12;
+            this.listClips.Location = new System.Drawing.Point(11, 37);
+            this.listClips.Name = "listClips";
+            this.listClips.Size = new System.Drawing.Size(513, 88);
+            this.listClips.TabIndex = 2;
+            // 
+            // tbOutputPath
+            // 
+            this.tbOutputPath.Location = new System.Drawing.Point(11, 147);
+            this.tbOutputPath.Name = "tbOutputPath";
+            this.tbOutputPath.Size = new System.Drawing.Size(396, 21);
+            this.tbOutputPath.TabIndex = 0;
+            this.tbOutputPath.Text = "d:/clips/outputclip1.mp4";
+            // 
+            // btnOutBrowse
+            // 
+            this.btnOutBrowse.Location = new System.Drawing.Point(455, 145);
+            this.btnOutBrowse.Name = "btnOutBrowse";
+            this.btnOutBrowse.Size = new System.Drawing.Size(69, 23);
+            this.btnOutBrowse.TabIndex = 1;
+            this.btnOutBrowse.Text = "...";
+            this.btnOutBrowse.UseVisualStyleBackColor = true;
+            this.btnOutBrowse.Click += new System.EventHandler(this.btnOutBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "output";
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            "--Select--",
+            "MP4"});
+            this.cbFormat.Location = new System.Drawing.Point(71, 174);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(121, 20);
+            this.cbFormat.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "format";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "size";
+            // 
+            // cbResolution
+            // 
+            this.cbResolution.FormattingEnabled = true;
+            this.cbResolution.Items.AddRange(new object[] {
+            "--Select--",
+            "\'hd1080\' 1920x1080"});
+            this.cbResolution.Location = new System.Drawing.Point(264, 174);
+            this.cbResolution.Name = "cbResolution";
+            this.cbResolution.Size = new System.Drawing.Size(121, 20);
+            this.cbResolution.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 336);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(691, 22);
+            this.statusStrip1.TabIndex = 6;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(543, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "Ready";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 200);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(445, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(38, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(95, 200);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(489, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(35, 23);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "-";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "history";
+            // 
+            // listHistory
+            // 
+            this.listHistory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listHistory.FullRowSelect = true;
+            this.listHistory.Location = new System.Drawing.Point(15, 261);
+            this.listHistory.Name = "listHistory";
+            this.listHistory.Size = new System.Drawing.Size(509, 59);
+            this.listHistory.TabIndex = 7;
+            this.listHistory.UseCompatibleStateImageBehavior = false;
+            this.listHistory.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 347;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Note";
+            this.columnHeader2.Width = 132;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 358);
+            this.Controls.Add(this.listHistory);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.cbResolution);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbFormat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listClips);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnOutBrowse);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbOutputPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listClips;
+        private System.Windows.Forms.TextBox tbOutputPath;
+        private System.Windows.Forms.Button btnOutBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbResolution;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listHistory;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ffmpegTestApp
+{
+    public partial class ConcatForm : Form
+    {
+        Process pFFmpeg = new Process();
+        string srcFileList = "filelist.ffc";
+        public ConcatForm()
+        {
+            InitializeComponent();
+            this.cbFormat.SelectedIndex = 1;
+            this.cbResolution.SelectedIndex = 1;
+            
+        }
+
+        private string ConvertBackSlash(string ori)
+        {
+            string outstring ="";
+            outstring = ori.Replace('\\', '/');
+            return outstring;
+        }
+
+        private bool MakeFileList()
+        {
+            try
+            {
+                StreamWriter outputFile = new StreamWriter(srcFileList);
+                outputFile.WriteLine("ffconcat version 1.0");
+
+                foreach (var itemPath in this.listClips.Items)
+                {
+                    outputFile.WriteLine("file "+ConvertBackSlash(itemPath.ToString()));
+                }
+                outputFile.Close();
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private int GetTotalDuration()
+        {
+            Process pFfprobe = new Process();
+            ProcessStartInfo psiProcInfo = new ProcessStartInfo();
+            
+            psiProcInfo.FileName = Application.StartupPath + "\\x64\\ffprobe.exe";
+            psiProcInfo.UseShellExecute = false;
+            psiProcInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            psiProcInfo.RedirectStandardError = true;
+            psiProcInfo.RedirectStandardOutput = true;
+            psiProcInfo.CreateNoWindow = true;
+           
+            double totalDur = 0;
+            using (StreamReader sr = new StreamReader(srcFileList))
+            {
+                string afilePath;
+                while ((afilePath = sr.ReadLine()) != null)
+                {
+                    // substring "file "
+                    string strFFMPEGCmd = "-v quiet -of csv=p=0 -show_format_entry duration " + afilePath.Substring(4);
+
+                    psiProcInfo.Arguments = strFFMPEGCmd;
+                    pFfprobe.StartInfo = psiProcInfo;
+                    pFfprobe.Start();
+                    StreamReader srFFmpeg = pFfprobe.StandardOutput;
+                    string strFFMPEGOut = srFFmpeg.ReadLine();
+                    totalDur += Convert.ToDouble(strFFMPEGOut);
+                }
+            }
+            return (int)((totalDur+1)*1000);
+        }
+
+        private void ConcatenateClips(string dstFile, int totalDuration)
+        {
+            try
+            {
+                string strFFMPEGOut;
+                ProcessStartInfo psiProcInfo = new ProcessStartInfo();
+                TimeSpan estimatedTime = TimeSpan.MaxValue;
+                StreamReader srFFmpeg;
+                string strFFMPEGCmd = "-f concat -safe 0 -i " + srcFileList + " -c:v libx264 -y " + dstFile;
+                //psiProcInfo.FileName = Application.StartupPath + ((IntPtr.Size == 8) ? "\\x64" : "\\x86") + "\\ffmpeg.exe";
+                psiProcInfo.FileName = Application.StartupPath + "\\x64\\ffmpeg.exe";
+                psiProcInfo.Arguments = strFFMPEGCmd;
+                psiProcInfo.UseShellExecute = false;
+                psiProcInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                psiProcInfo.RedirectStandardError = true;
+                psiProcInfo.RedirectStandardOutput = true;
+                psiProcInfo.CreateNoWindow = true;
+                
+                pFFmpeg.StartInfo = psiProcInfo;
+                pFFmpeg.Start();
+                srFFmpeg = pFFmpeg.StandardError;
+
+                do
+                {
+                    strFFMPEGOut = srFFmpeg.ReadLine();
+                    if (strFFMPEGOut != null)
+                    {
+                        string time = "time=";
+                        int startPos = strFFMPEGOut.IndexOf(time);
+                        if (startPos != -1)
+                        {
+                            string text = strFFMPEGOut.Substring(startPos + time.Length);
+                            int pos = text.IndexOf(" ");
+                            string current = text.Substring(0, pos);
+
+                            TimeSpan currentTime = TimeSpan.Parse(current);
+
+                            int progresss = (int)currentTime.TotalMilliseconds;
+                            this.BeginInvoke(new MethodInvoker(() =>
+                            {
+                                this.toolStripProgressBar1.Value = progresss;
+
+                            }));
+                        }
+                        this.BeginInvoke(new MethodInvoker(() =>
+                        {
+                            this.toolStripStatusLabel1.Text = strFFMPEGOut;
+                        }));
+                        
+                        
+                    }
+                } while (pFFmpeg.HasExited == false);
+            }
+            catch(Exception e)
+            {
+                this.BeginInvoke(new MethodInvoker(() =>
+                {
+                    Console.WriteLine(e);
+                    this.toolStripStatusLabel1.Text = "failed : " + e.Message;
+                    string[] history = { dstFile, "failed" };
+                    this.listHistory.Items.Add(new ListViewItem(history));
+                    this.btnStart.Enabled = true;
+                    this.btnStop.Enabled = false;
+                }));
+            }
+            finally
+            {
+                if (pFFmpeg.ExitCode != -1)
+                {
+                    this.BeginInvoke(new MethodInvoker(() =>
+                    {
+                        this.toolStripStatusLabel1.Text = "Done";
+                        this.btnStart.Enabled = true;
+                        this.btnStop.Enabled = false;
+                        this.toolStripProgressBar1.Value = totalDuration;
+                        string[] history = { dstFile, "Done" };
+                        this.listHistory.Items.Add(new ListViewItem(history));
+                    }));
+                }
+            }
+        }
+
+        // events
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "mxf| *.mxf|mp4|*.mp4|All(*.*)|*.*";
+            dlg.Multiselect = true;
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string strFileName in dlg.FileNames)
+                {
+                    this.listClips.Items.Add(strFileName);
+                }
+            }
+        }
+
+        private void btnOutBrowse_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "mp4|*.mp4|All(*.*)|*.*";
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                this.tbOutputPath.Text = ConvertBackSlash(dlg.FileName);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if(this.listClips.SelectedIndex < 0)
+            {
+                return;
+            }
+            else
+            {
+                int idx = this.listClips.SelectedIndex;
+                this.listClips.Items.Remove(this.listClips.SelectedItem);
+                if( this.listClips.Items.Count >0)
+                {
+                    if (idx == 0)
+                    {
+                        this.listClips.SelectedIndex = 0;
+                    }
+                    else
+                        this.listClips.SelectedIndex = (idx-1);
+                }
+                
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            pFFmpeg.Kill();
+            string[] history = { this.tbOutputPath.Text, "stopped" };
+            this.listHistory.Items.Add(new ListViewItem(history));
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+            this.toolStripProgressBar1.Value = 0;
+            this.toolStripStatusLabel1.Text = "Stopped";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+            this.toolStripProgressBar1.Value = 0;
+            this.toolStripStatusLabel1.Text = "";
+
+            MakeFileList();
+            this.toolStripProgressBar1.Maximum = GetTotalDuration();
+
+
+            this.toolStripStatusLabel1.Text = "concatenate...";
+
+            ThreadPool.QueueUserWorkItem((object state) =>
+            {
+                string outFile = this.tbOutputPath.Text;
+                ConcatenateClips(outFile, this.toolStripProgressBar1.Maximum);
+            });
+        }
+
+        private void ResetControls(bool enabled)
+        {
+            this.btnStart.Enabled = enabled;
+            this.btnStop.Enabled = !enabled;
+            this.toolStripProgressBar1.Value = 0;
+            this.toolStripStatusLabel1.Text = "";
+        }
+    }
+}
